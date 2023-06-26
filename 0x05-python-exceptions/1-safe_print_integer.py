c07ed8d06d2f962b6111202f18405ee0a2826912
redachahdi <reda.elkhattabi01@gmail.com>
2023-06-26 02:15:30 -0700

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-   that  prints the first x elements in the  list 
+    that prints the integer with .
 
     """
 
 
-def safe_print_list_integers(my_list=[], x=0):
-    nb = 0
-    for y in range(0, x):
-        try:
-            print("{:d}".format(my_list[y]), end="")
-            nb += 1
-        except(TypeError, ValueError):
-            pass
-    print()
-    return(nb)
+def safe_print_integer(value):
+    try:
+        print('{:d}'.format(value))
+        return(True)
+    except:
+        return False
